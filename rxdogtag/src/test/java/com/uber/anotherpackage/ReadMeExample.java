555a42a8959362fc22f81f85b44178b15776a21e
Zac Sweers <pandanomic@gmail.com>
2019-04-07 18:48:44 -0700

    Run spotless

--- rxdogtag/src/test/java/com/uber/anotherpackage/ReadMeExample.java
+++ rxdogtag/src/test/java/com/uber/anotherpackage/ReadMeExample.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2019. Uber Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.uber.anotherpackage;
 
 import com.uber.rxdogtag.RxDogTag;
@@ -15,7 +30,9 @@ import org.junit.Test;
 @Ignore
 public class ReadMeExample {
 
-  @Rule public AggressiveUncaughtExceptionHandlerRule handlerRule = new AggressiveUncaughtExceptionHandlerRule();
+  @Rule
+  public AggressiveUncaughtExceptionHandlerRule handlerRule =
+      new AggressiveUncaughtExceptionHandlerRule();
 
   @After
   public void tearDown() {
@@ -25,8 +42,7 @@ public class ReadMeExample {
   @Test
   public void simpleSubscribe() {
     RxDogTag.install();
-    Observable.error(new RuntimeException("Unhandled error!"))
-        .subscribe();
+    Observable.error(new RuntimeException("Unhandled error!")).subscribe();
   }
 
   @Test
