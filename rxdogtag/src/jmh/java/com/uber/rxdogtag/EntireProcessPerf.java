7ec62f25d8442d45d91f4be7fd7390fcb71cf7fa
Daniel Lew <dlew@users.noreply.github.com>
2019-05-16 22:07:15 -0500

    Added some simple JMH benchmarks (#23)
    * Added JMH Gradle plugin to project
    
    * Added some basic performance tests

---
+++ rxdogtag/src/jmh/java/com/uber/rxdogtag/EntireProcessPerf.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2019. Uber Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.uber.rxdogtag;
+
+import com.uber.rxdogtag.util.PerfConsumer;
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.plugins.RxJavaPlugins;
+import java.util.Arrays;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+/** Measures the time it takes to both subscribe and consume events. */
+@BenchmarkMode(Mode.Throughput)
+@State(Scope.Thread)
+public class EntireProcessPerf {
+
+  @Param({"1", "1000", "1000000"})
+  public int times;
+
+  @Param({"false", "true"})
+  public boolean installRxDogTag;
+
+  Flowable<Integer> flowable = Flowable.empty();
+  Observable<Integer> observable = Observable.empty();
+
+  @Setup
+  public void setup(Blackhole bh) {
+    if (installRxDogTag) {
+      RxDogTag.install();
+    }
+
+    Integer[] array = new Integer[times];
+    Arrays.fill(array, 777);
+
+    flowable = Flowable.fromArray(array);
+
+    observable = Observable.fromArray(array);
+  }
+
+  @TearDown
+  public void tearDown() {
+    RxJavaPlugins.reset();
+  }
+
+  @Benchmark
+  public Disposable flowable(Blackhole bh) {
+    return flowable.subscribe(new PerfConsumer(bh));
+  }
+
+  @Benchmark
+  public Disposable observable(Blackhole bh) {
+    return observable.subscribe(new PerfConsumer(bh));
+  }
+}
