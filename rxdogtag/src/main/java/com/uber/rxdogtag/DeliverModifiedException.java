aa0685c62b720e80f57e899afb9b4e8c24e39342
Zac Sweers <zac.sweers@gmail.com>
2019-08-20 16:17:19 -0400

    Add DeliverModifiedException marker type

---
+++ rxdogtag/src/main/java/com/uber/rxdogtag/DeliverModifiedException.java
@@ -0,0 +1,11 @@
+package com.uber.rxdogtag;
+
+/**
+ * A marker type to indicate that RxDogTag's should pass the decorated stacktrace to
+ * {@link #onError(Throwable)}. Note that this will always be tried directly, and no guarded
+ * delegate behavior will be attempted even if
+ * {@link RxDogTag.Builder#guardObserverCallbacks(boolean)} is enabled in configuration.
+ */
+public interface DeliverModifiedException extends TryOnError {
+
+}
